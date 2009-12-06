db54be3d7a93925e555f1409071e3172babcd343
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-12-06 10:45:32 -0500

    Fixed project name to remove inadvertent inclusion of space character.

--- src/IConnection.java
+++ src/IConnection.java
@@ -1,4 +1,21 @@
 
+/**
+ * An atomic network element.  Represents an information propagation mechanism.
+ * 
+ * @author Alex Maskovyak
+ *
+ */
 public interface IConnection {
 
+	/**
+	 * Connects a series of nodes.
+	 * @param nodes to connect.
+	 */
+	public void connect(INode... nodes);
+	
+	/**
+	 * Connects a single node.
+	 * @param node to connect.
+	 */
+	public void connect(INode node);
 }
--- src/INode.java
+++ src/INode.java
@@ -1,4 +1,11 @@
 
+/**
+ * An atomic network element.  Represents an information source and sink.
+ * 
+ * @author Alex Maskovyak
+ *
+ */
 public interface INode {
 
+	
 }
