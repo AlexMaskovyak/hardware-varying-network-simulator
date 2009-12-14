cda828cd2d0d9613d1df2f38719d3c5fe3285bda
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-12-13 23:35:59 -0500

    Fleshing out the interfaces a touch.

--- src/IDistributedAlgorithm.java
+++ src/IDistributedAlgorithm.java
@@ -1,4 +1,13 @@
 
 public interface IDistributedAlgorithm {
 
+	/**
+	 * Distribute data to elements in the network.
+	 */
+	public void distribute();
+	
+	/**
+	 * Read back data from sources in the network.
+	 */
+	public void read();
 }
