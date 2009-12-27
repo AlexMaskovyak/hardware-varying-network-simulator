1de49d02ca7e6ab1716fefe2cb07edfaf28dfedd
user <user@localhost>
2009-12-26 22:21:46 -0500

    Adding sending and receiving logic.

--- src/INode.java
+++ src/INode.java
@@ -7,7 +7,28 @@
  */
 public interface INode {
 
-	public void connect(IConnection connect);
+	/**
+	 * Connects this node to a connector and the connector to the node.
+	 * @param connection to which to join.
+	 */
+	public void connect(IConnection connection);
 	
+	/**
+	 * Receives data.
+	 * @param data to have this node receive.
+	 */
 	public void receive(IData data);
+	
+	/**
+	 * Sends data out to all connections.
+	 * @param data to send to connected items.
+	 */
+	public void send(IData data);
+	
+	/**
+	 * Sends data out across the specified connection.
+	 * @param data to send to the connected item.
+	 * @param connection to receive the data.
+	 */
+	public void send(IData data, IConnection connection);
 }
